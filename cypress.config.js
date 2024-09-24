92f8783f60b480c1d1af2840ac304ec4ca367ddf
Stoyan S <stoyan@protohologram.com>
2024-09-23 22:52:15 -0700

    junit

--- cypress.config.js
+++ cypress.config.js
@@ -1,16 +1,14 @@
-const { defineConfig } = require('cypress');
+import { defineConfig } from 'cypress';
 
-module.exports = defineConfig({
+export default defineConfig({
   e2e: {
     setupNodeEvents(on, config) {
       // implement node event listeners here
     },
-    reporter: 'mochawesome',
+    reporter: 'mocha-junit-reporter',
     reporterOptions: {
-      reportDir: 'cypress/reports/mocha',
-      overwrite: false,
-      html: true,
-      json: true,
+      mochaFile: 'cypress/reports/junit/results-[hash].xml',
+      toConsole: true
     },
   },
 });
