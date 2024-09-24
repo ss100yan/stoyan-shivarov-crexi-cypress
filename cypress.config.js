e7454215a970a9caeea4f7518f948619749b3ecc
Stoyan S <stoyan@protohologram.com>
2024-09-23 19:49:44 -0700

    Add Cypress configuration with mochawesome reporter

---
+++ cypress.config.js
@@ -0,0 +1,16 @@
+const { defineConfig } = require('cypress');
+
+module.exports = defineConfig({
+  e2e: {
+    setupNodeEvents(on, config) {
+      // implement node event listeners here
+    },
+    reporter: 'mochawesome',
+    reporterOptions: {
+      reportDir: 'cypress/reports/mocha',
+      overwrite: false,
+      html: true,
+      json: true,
+    },
+  },
+});
