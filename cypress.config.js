554992a4b35afef711663d54a3ed48ece2fa3407
Stoyan S <stoyan@protohologram.com>
2024-09-23 23:21:51 -0700

    cchange

--- cypress.config.js
+++ cypress.config.js
@@ -1,10 +1,19 @@
-module.exports = {
-  projectId: "tpywq4",
-  // ...rest of the Cypress project config
+const { defineConfig } = require('cypress');
 
-
-
-  
-    
-
-}
+module.exports = defineConfig({
+  projectId: 'tpywq4',
+  e2e: {
+    setupNodeEvents(on, config) {
+      // implement node event listeners here
+    },
+    baseUrl: 'https://www.crexi.com',  // Optional: set a base URL for tests
+    specPattern: 'cypress/e2e/**/*.cy.{js,jsx,ts,tsx}', // Ensures it looks for test specs in your e2e folder
+  },
+  reporter: 'mochawesome',
+  reporterOptions: {
+    reportDir: 'cypress/reports/mocha',
+    overwrite: false,
+    html: true,
+    json: true,
+  },
+});
